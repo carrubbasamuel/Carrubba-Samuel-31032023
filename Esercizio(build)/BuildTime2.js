df58f5545c65f83c119fc7ae9c959c9e9357ca06
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-04-01 02:46:59 +0200

    Finish.

--- Esercizio(build)/BuildTime2.js
+++ Esercizio(build)/BuildTime2.js
@@ -29,5 +29,76 @@ const amy = {
   isAmbassador: false,
 }
 
-const prices = [34, 5, 2]
-const shippingCost = 50
+const prices = [3.99, 4.99, 29.99, 99.99, 19.99, 25.00];
+const shippingCost = 50;
+
+
+
+/*-----------Genera indici casuali dell'array prices per riempire il carrello--------*/
+
+/*-------Carrello di Marco--------*/
+
+let cartArrayMarco = [
+  prices[parseInt(Math.random()*prices.length-1)], 
+  prices[parseInt(Math.random()*prices.length-1)],
+  prices[parseInt(Math.random()*prices.length-1)]
+];
+
+
+
+
+/*--------Carrello di Paul------- */
+let cartArrayPaul = [
+  prices[parseInt(Math.random()*prices.length-1)], 
+  prices[parseInt(Math.random()*prices.length-1)],
+  prices[parseInt(Math.random()*prices.length-1)]
+];
+
+
+
+/*-----------Carrello di Amy---------*/
+
+let cartArrayAmy = [
+  prices[parseInt(Math.random()*prices.length-1)], 
+  prices[parseInt(Math.random()*prices.length-1)]
+];
+
+
+/*---------------Questa funzione calcola il costo totale della spedizione per un utente dato,
+  sulla base di un carrello di acquisti e di un costo di spedizione. La funzione inizierà calcolando la somma
+  dei prezzi degli elementi nel carrello. Se l'utente è un ambasciatore, calcolerà il 30% di sconto sulla somma totale e sottrarrà lo sconto. 
+  Se la somma finale è superiore a 100,la spedizione sarà gratuita. Infine, il costo totale della spedizione
+  sarà la somma totale scontata più il costo di spedizione.------------------ */
+
+
+function costOfTotalShipping(user, cartArray , shippingCost) {
+  let total =0;
+
+  for(let i=0; i<cartArray.length; i++){
+    total += cartArray[i];
+  }
+
+  let scount = 0;
+  
+  if (user.isAmbassador)scount = total * 0.3;
+
+  total -= scount;
+
+  if (total > 100) shippingCost = 0;
+
+
+  return parseFloat(total.toFixed(2)) + parseFloat(shippingCost.toFixed(2));
+}
+
+/*-------------------consol.log() dei relativi carrelli con applicati i vantaggi dello sconto e spedizione gratuita ove necessario---------------------*/
+
+console.log(
+  "Marco in base al suo carrello pagherà:   ",  costOfTotalShipping(marco, cartArrayMarco, shippingCost), cartArrayMarco
+); 
+console.log(
+  "Paul in base al suo carrello pagherà:   ", costOfTotalShipping(paul, cartArrayPaul, shippingCost), cartArrayPaul
+); 
+console.log(
+  "Amy in base al suo carrello pagherà:   ", costOfTotalShipping(amy, cartArrayAmy, shippingCost), cartArrayAmy
+);
+
